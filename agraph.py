aaaf3a8f0c13a3eb1023bb0f7cbd47e9e89764ac
Hoang Xuan Phu <phunehehe@gmail.com>
2012-03-08 00:24:37 +0700

    the simplest of graphs

--- agraph.py
+++ agraph.py
@@ -2,14 +2,35 @@
 
 from subprocess import Popen
 from subprocess import PIPE
-from datetime import datetime
+import json
 
 
-for line in Popen(['atop', '-PDSK', '-r', '/var/log/atop.log.1'], stdout=PIPE).stdout:
+timestamps = []
+reads = []
+writes = []
+
+output = Popen(['atop', '-PDSK', '-r', '/var/log/atop.log.1'], stdout=PIPE).stdout
+skip_first = True
+
+for line in output:
     line = line.strip()
     if line in ('SEP', 'RESET'):
         continue
+    # First readings are ridiculously high
+    if skip_first:
+        skip_first = False
+        continue
     tokens = line.split(' ')
-    print tokens
     label, host, timestamp_str, _, _, interval, device, milliseconds, read, sectors_read, write, sectors_written = tokens
-    timestamp = datetime.fromtimestamp(int(timestamp_str))
+    timestamp = int(timestamp_str)
+    timestamps.append(timestamp)
+    reads.append(int(read))
+    writes.append(int(write))
+
+
+f = open('data.js', 'w')
+f.write('data = ' + json.dumps({
+    'timestamps': timestamps,
+    'reads': reads,
+    'writes': writes,
+}))
