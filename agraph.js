fa2859ddcf8ed837a7f7042adfa475ce7b117b08
Hoang Xuan Phu <phunehehe@gmail.com>
2012-03-11 13:43:27 +0700

    split graphs by device

--- agraph.js
+++ agraph.js
@@ -1,16 +1,33 @@
-var timestamps = [];
-var reads = [];
-var writes = [];
-
-for (var i = 0; i < data.length; i++) {
-    var tuple = data[i];
-    console.log(tuple);
-    timestamps.push(tuple[0]);
-    reads.push(tuple[1]);
-    writes.push(tuple[2]);
-}
-
 window.onload = function () {
-    var r = Raphael(0, 0, 800, 480);
-    r.linechart(0, 0, 800, 480, timestamps, [reads, writes], {shade: true});
+
+    var screen_width = screen.width;
+    var screen_height = screen.height;
+
+    var r = Raphael(0, 0, screen_width, screen_height);
+
+    var graph_device = function(x, y, width, height, data) {
+
+        var timestamps = [];
+        var reads = [];
+        var writes = [];
+
+        // Start with 1 to skip first entry with accumulated readings
+        for (var i = 1, j = data.length; i < j; i++) {
+            var tuple = data[i];
+            timestamps.push(tuple[0]);
+            reads.push(tuple[1]);
+            writes.push(tuple[2]);
+        }
+
+        r.linechart(x, y, width, height, timestamps, [reads, writes], {shade: true});
+    }
+
+    var y = 0;
+    var y_offset = screen_height / Object.keys(data).length;
+    var graph_height = y_offset - 10;
+
+    for (device in data) {
+        graph_device(0, y, 800, graph_height, data[device]);
+        y += y_offset;
+    }
 };
--- agraph.py
+++ agraph.py
@@ -8,20 +8,21 @@ import json
 log_file = Popen(['atop', '-PDSK', '-r', '/var/log/atop.log.1'], stdout=PIPE).stdout
 
 
-data = []
-skip_first = True
+data = {}
 
 for line in log_file:
+
     line = line.strip()
     if line in ('SEP', 'RESET'):
         continue
-    # First readings are ridiculously high
-    if skip_first:
-        skip_first = False
-        continue
+
     tokens = line.split(' ')
     label, host, timestamp, _, _, interval, device, milliseconds, read, sectors_read, write, sectors_written = tokens
-    data.append((int(timestamp), int(read), int(write)))
+
+    if device not in data:
+        data[device] = []
+    data[device].append((int(timestamp), int(read), int(write)))
+
 
 f = open('data.js', 'w')
 f.write('data = ' + json.dumps(data))
