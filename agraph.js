352ced1b7e60997390571c98dd9e077b297a3c2a
Hoang Xuan Phu <phunehehe@gmail.com>
2012-03-11 14:50:56 +0700

    graph is nice enough with raphael

--- agraph.js
+++ agraph.js
@@ -1,9 +1,19 @@
 window.onload = function () {
 
-    var screen_width = screen.width;
-    var screen_height = screen.height;
+    var documentElement = document.documentElement;
+    var screen_width = documentElement.clientWidth;
+    var screen_height = documentElement.clientHeight;
 
-    var r = Raphael(0, 0, screen_width, screen_height);
+    var screen_padding = 10;
+    var svg_width = screen_width - screen_padding * 2;
+    var svg_height = screen_height - screen_padding * 2;
+
+    var r = Raphael(screen_padding, screen_padding, svg_width, svg_height);
+
+    var graph_options = {
+        shade: true,
+        axis: '0 0 1 1',
+    };
 
     var graph_device = function(x, y, width, height, data) {
 
@@ -19,15 +29,18 @@ window.onload = function () {
             writes.push(tuple[2]);
         }
 
-        r.linechart(x, y, width, height, timestamps, [reads, writes], {shade: true});
+        r.linechart(x, y, width, height, timestamps, [reads, writes], graph_options);
     }
 
-    var y = 0;
-    var y_offset = screen_height / Object.keys(data).length;
-    var graph_height = y_offset - 10;
+    var graph_height_outer = svg_height / Object.keys(data).length;
+    var graph_padding = 10;
+    var graph_height_inner = graph_height_outer - graph_padding * 2;
+    var graph_width = screen_width;
 
+    var y = 0;
     for (device in data) {
-        graph_device(0, y, 800, graph_height, data[device]);
-        y += y_offset;
+        graph_device(50, y, graph_width, graph_height_inner, data[device]);
+        r.text(graph_width / 2, y + 10, 'Disk usage for ' + device);
+        y += graph_height_outer;
     }
 };
