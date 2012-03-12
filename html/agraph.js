9b48df34e347027b7abe608a0dce9e4c89fd5f1e
Hoang Xuan Phu <phunehehe@gmail.com>
2012-03-13 00:26:14 +0700

    with zooming, but somehow it doesn't work with multiple graphs

--- html/agraph.js
+++ html/agraph.js
@@ -5,7 +5,7 @@ Date.prototype.format = function(formatString) {
 }
 
 
-var dataToSeries = function(data) {
+function dataToSeries(data) {
 
     var reads = [];
     var writes = [];
@@ -39,6 +39,7 @@ $(document).ready(function() {
         var series = dataToSeries(data[device]);
 
         var options = {
+            selection : { mode : 'x' },
             xaxis: {
                 noTicks: 20,
                 tickFormatter: function(millis) {
@@ -48,6 +49,35 @@ $(document).ready(function() {
             }
         }
 
-        Flotr.draw(container, series, options);
+        function drawGraph(opts) {
+            // Clone the options, so the 'options' variable always keeps intact.
+            var o = Flotr._.extend(Flotr._.clone(options), opts || {});
+            return Flotr.draw(
+                container,
+                series,
+                o
+            );
+        }
+
+        drawGraph();
+
+        Flotr.EventAdapter.observe(container, 'flotr:select', function(area) {
+            // Draw graph with new area
+            f = drawGraph({
+                xaxis: {
+                    min: area.x1,
+                    max: area.x2
+                },
+                yaxis: {
+                    min: area.y1,
+                    max: area.y2
+                }
+            });
+        });
+
+        // When graph is clicked, draw the graph with default area.
+        Flotr.EventAdapter.observe(container, 'flotr:click', function() { drawGraph(); });
+
+        break;
     }
 });
